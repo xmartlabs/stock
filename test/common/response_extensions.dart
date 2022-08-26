1d3c62c3ea1f6ad843576099ef41348b58f256ec
Matías Irland <mirland@xmartlabs.com>
2022-08-26 20:35:38 -0300

    Remove freezed dependency (#5)

--- test/common/response_extensions.dart
+++ test/common/response_extensions.dart
@@ -2,6 +2,6 @@ import 'package:stock/store_response.dart';
 
 extension ResponseExtensions<T> on StoreResponse<T> {
   StoreResponse<T> removeStacktraceIfNeeded() => this is StoreResponseError<T>
-      ? (this as StoreResponseError<T>).copyWith(stackTrace: null)
+      ? StoreResponse.error(origin, (this as StoreResponseError<T>).error)
       : this;
 }
